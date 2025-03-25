bacb0245f973bb3058ee40e07ee514cfdeb0dd91
prasanna kothoju <134475079+prasannakothoju@users.noreply.github.com>
2025-03-25 11:47:51 +0530

    Update basicHashMap.java

--- basicHashMap.java
+++ basicHashMap.java
@@ -3,8 +3,6 @@ import java.util.*;
 
 public class Main{
     
-    
-    
     static Map<String, String> m = new HashMap<>();
     public static void mapping(){
     m.put("sam","9010880281");
@@ -14,7 +12,23 @@ public class Main{
     
     public static void main(String[] args){
         mapping();
-        System.out.println(m.get("sam"));
+        
+        //to print only 1 value 
+        //System.out.println(m.get("sam"));
+        
+        //if the data is in thousands how to print all?
+        // Set<String> keys = m.keySet();
+        // for(String i : keys){
+        //     System.out.println(i+":"+m.get(i));
+        // }
+        
+        //this is the another method
+        Set<Map.Entry<String,String>> values = m.entrySet();
+        for(Map.Entry<String,String> e:values ){
+            System.out.println(e.getKey()+":"+e.getValue());
+        }
+        
+        
     }
     
 }
